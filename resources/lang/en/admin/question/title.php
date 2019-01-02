a1c5608a6a3d164733daf5388c080903c2a762e7
Paco Orozco <paco@pacoorozco.info>
2019-01-02 16:31:37 +0100

    Apply fixes from StyleCI (#29)

--- resources/lang/en/admin/question/title.php
+++ resources/lang/en/admin/question/title.php
@@ -1,18 +1,18 @@
 <?php
 
 return [
-    'question_management' => 'Question management',
+    'question_management'          => 'Question management',
     'question_management_subtitle' => 'create and edit questions',
 
-    'question_show' => 'Question details',
+    'question_show'         => 'Question details',
     'create_a_new_question' => 'Create new question',
-    'question_edit' => 'Edit question',
-    'question_delete' => 'Delete question',
-    
+    'question_edit'         => 'Edit question',
+    'question_delete'       => 'Delete question',
+
     'general_section' => 'General',
-    'answer_section' => 'Answers',
+    'answer_section'  => 'Answers',
     'publish_section' => 'Publish',
-    'tags_section' => 'Tags',
-    'badges_section' => 'Associated badges',
-    'other_section' => 'Other information',
+    'tags_section'    => 'Tags',
+    'badges_section'  => 'Associated badges',
+    'other_section'   => 'Other information',
 ];
