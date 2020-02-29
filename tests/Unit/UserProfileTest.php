59a3ece89f6ebfb6975f28cd61d1c9a9b3499d02
pacoorozco <paco@pacoorozco.info>
2020-02-29 19:50:35 +0100

    Test for UserProfile  model
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ tests/Unit/UserProfileTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use Gamify\UserProfile;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Tests\ModelTestCase;
+
+class UserProfileTest extends ModelTestCase
+{
+    public function test_contains_valid_fillable_properties()
+    {
+        $m = new UserProfile();
+        $this->assertEquals([
+            'bio',
+            'url',
+            'phone',
+            'date_of_birth',
+            'gender',
+            'twitter',
+            'facebook',
+            'linkedin',
+            'github',
+        ], $m->getFillable());
+    }
+
+    public function test_contains_valid_casts_properties()
+    {
+        $m = new UserProfile();
+        $this->assertEquals([
+            'id' => 'int',
+            'bio' => 'string',
+            'url' => 'string',
+            'phone' => 'string',
+            'date_of_birth' => 'string',
+            'gender' => 'string',
+            'twitter' => 'string',
+            'facebook' => 'string',
+            'linkedin' => 'string',
+            'github' => 'string',
+        ], $m->getCasts());
+    }
+
+    public function test_user_relation()
+    {
+        $m = new UserProfile();
+        $r = $m->user();
+        $this->assertInstanceOf(BelongsTo::class, $r);
+    }
+}
