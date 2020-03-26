4b56c0b3c326d151bbbda1b02e80ae9d0caec2e6
pacoorozco <paco@pacoorozco.info>
2020-03-26 11:24:02 +0100

    Fix doctrine error when doing migration
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- database/migrations/2020_03_25_114812_prepare_users_table_for_social_authentication.php
+++ database/migrations/2020_03_25_114812_prepare_users_table_for_social_authentication.php
@@ -2,10 +2,23 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class PrepareUsersTableForSocialAuthentication extends Migration
 {
+    /**
+     * PrepareUsersTableForSocialAuthentication constructor.
+     *
+     * Forces Doctrine to treat enum columns as string.
+     */
+    public function __construct()
+    {
+        DB::getDoctrineSchemaManager()
+            ->getDatabasePlatform()
+            ->registerDoctrineTypeMapping('enum', 'string');
+    }
+
     /**
      * Run the migrations.
      *
