57b0d16b7a01857d248666f40deca0430f83ae7e
pacoorozco <paco@pacoorozco.info>
2020-03-27 14:19:15 +0100

    Fix wrong redirection after login
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, etc.
