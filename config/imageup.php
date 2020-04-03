81e29ef80e771a89025f6969b2a5f2936e7cf1c6
Paco Orozco <paco@pacoorozco.info>
2020-04-03 11:16:39 +0200

    Apply fixes from StyleCI (#93)
    [ci skip] [skip ci]

--- config/imageup.php
+++ config/imageup.php
@@ -3,30 +3,30 @@
 return [
 
     /**
-     * Default upload storage disk
+     * Default upload storage disk.
      */
     'upload_disk' => 'public',
 
     /**
      * Default Image upload directory on the disc
-     * eg. 'uploads' or 'user/avatar'
+     * eg. 'uploads' or 'user/avatar'.
      */
     'upload_directory' => 'uploads',
 
     /**
      * Auto upload images from incoming Request if same named field or
      * file_input field on option present upon model update and create.
-     * can be override in individual field options
+     * can be override in individual field options.
      */
     'auto_upload_images' => true,
 
     /**
-     * It will auto delete images once record is deleted from database
+     * It will auto delete images once record is deleted from database.
      */
     'auto_delete_images' => true,
 
     /**
-     * Set an image quality
+     * Set an image quality.
      */
-    'resize_image_quality' => 80
+    'resize_image_quality' => 80,
 ];
