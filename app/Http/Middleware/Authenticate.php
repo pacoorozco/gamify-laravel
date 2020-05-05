c1bff1deda6e69a937f4b74a93cb8934ff38e82f
pacoorozco <paco@pacoorozco.info>
2020-05-05 10:23:46 +0200

    Comment non used code
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,10 +12,13 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
+    /* This code is left here for future use.
+
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
     }
+    */
 }
