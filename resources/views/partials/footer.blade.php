e71502ceba507ed78f1352eb470a5f84831c465d
pacoorozco <paco@pacoorozco.info>
2021-04-20 17:24:19 +0200

    Add specific version in admin section footer
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,8 +3,6 @@
         <div class="pull-right hidden-xs">
             Just play. Have fun. Enjoy the game.
         </div>
-        <strong>Copyright &copy;
-            2014-{{ date("Y") }} <a href="https://pacoorozco.info" rel="nofollow">Paco Orozco</a></strong>.
-        Powered by <a href="https://github.com/pacoorozco/gamify-laravel" rel="nofollow">gamify v3</a>
+        Powered by <a href="https://github.com/pacoorozco/gamify-laravel" rel="nofollow">gamify</a>
     </div>
 </footer>
