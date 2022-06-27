135cef6d231723878431248ad694a02ad1c68c1c
pacoorozco <paco@pacoorozco.info>
2022-06-27 15:52:16 +0200

    Fix collation() for pgsql databases
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- database/migrations/2020_03_13_131607_create_taggable_table.php
+++ database/migrations/2020_03_13_131607_create_taggable_table.php
@@ -39,7 +39,7 @@ return new class extends Migration
         $driver = Schema::connection($connection)->getConnection()->getConfig('driver');
 
         $collation = match ($driver) {
-            'pgsql' => null,
+            'pgsql' => '',
             default => $charset . '_bin',
         };
 
