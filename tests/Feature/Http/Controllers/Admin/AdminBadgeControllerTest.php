e278fb260a9cbaf0558e1a736bc2b3278b7c867b
pacoorozco <paco@pacoorozco.info>
2022-07-06 07:46:29 +0200

    Add more tests and refactor controller
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- tests/Feature/Http/Controllers/Admin/AdminBadgeControllerTest.php
+++ tests/Feature/Http/Controllers/Admin/AdminBadgeControllerTest.php
@@ -25,10 +25,11 @@
 
 namespace Tests\Feature\Http\Controllers\Admin;
 
-use Gamify\Enums\BadgeActuators;
+use Gamify\Enums\Roles;
 use Gamify\Http\Middleware\OnlyAjax;
 use Gamify\Models\Badge;
 use Gamify\Models\User;
+use Generator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -36,101 +37,74 @@ class AdminBadgeControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+    private User $user;
+
     public function setUp(): void
     {
         parent::setUp();
 
-        /** @var User $admin */
-        $admin = User::factory()->admin()->create();
-        $this->actingAs($admin);
+        $this->user = User::factory()
+            ->create();
     }
 
     /** @test */
-    public function access_is_restricted_to_admins()
+    public function players_should_not_see_the_index_view(): void
     {
-        $badge = Badge::factory()->create();
-        $test_data = [
-            ['protocol' => 'GET', 'route' => route('admin.badges.index')],
-            ['protocol' => 'GET', 'route' => route('admin.badges.create')],
-            ['protocol' => 'POST', 'route' => route('admin.badges.store')],
-            ['protocol' => 'GET', 'route' => route('admin.badges.show', $badge)],
-            ['protocol' => 'GET', 'route' => route('admin.badges.edit', $badge)],
-            ['protocol' => 'PUT', 'route' => route('admin.badges.update', $badge)],
-            ['protocol' => 'GET', 'route' => route('admin.badges.delete', $badge)],
-            ['protocol' => 'DELETE', 'route' => route('admin.badges.destroy', $badge)],
-        ];
-
-        /** @var User $user */
-        $user = User::factory()->create();
-
-        foreach ($test_data as $test) {
-            $this->actingAs($user)
-                ->call($test['protocol'], $test['route'])
-                ->assertForbidden();
-        }
-
-        // Ajax routes needs to disable middleware
-        $this->actingAs($user)
-            ->withoutMiddleware(OnlyAjax::class)
-            ->get(route('admin.badges.data'))
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.index'))
             ->assertForbidden();
     }
 
     /** @test */
-    public function index_returns_proper_content()
+    public function admins_should_see_the_index_view()
     {
-        $this->get(route('admin.badges.index'))
-            ->assertOK()
+        $this->user->role = Roles::Admin;
+
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.index'))
+            ->assertSuccessful()
             ->assertViewIs('admin.badge.index');
     }
 
     /** @test */
-    public function create_returns_proper_content()
+    public function players_should_not_see_the_new_badge_form(): void
     {
-        $this->get(route('admin.badges.create'))
-            ->assertOk()
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.create'))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function admins_should_see_the_new_badge_form(): void
+    {
+        $this->user->role = Roles::Admin;
+
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.create'))
+            ->assertSuccessful()
             ->assertViewIs('admin.badge.create');
     }
 
     /** @test */
-    public function store_creates_an_object()
+    public function players_should_not_create_badges(): void
     {
         /** @var Badge $want */
         $want = Badge::factory()->make();
-        $input_data = [
-            'name' => $want->name,
-            'description' => $want->description,
-            'required_repetitions' => $want->required_repetitions,
-            'active' => true,
-            'actuators' => BadgeActuators::OnQuestionAnswered,
-        ];
 
-        $this->post(route('admin.badges.store'), $input_data)
-            ->assertRedirect(route('admin.badges.index'))
-            ->assertSessionHasNoErrors()
-            ->assertSessionHas('success');
-
-        $this->assertDatabaseHas(Badge::class, [
-            'name' => $want->name,
-            'description' => $want->description,
-            'required_repetitions' => $want->required_repetitions,
-            'active' => true,
-            'actuators' => BadgeActuators::OnQuestionAnswered,
-        ]);
-    }
-
-    /** @test */
-    public function store_returns_errors_on_invalid_data()
-    {
-        /** @var Badge $want */
-        $want = Badge::factory()->make();
-        $invalid_input_data = [
-            'name' => $want->name,
-        ];
-
-        $this->post(route('admin.badges.store'), $invalid_input_data)
-            ->assertSessionHasErrors()
-            ->assertSessionHas('errors');
+        $this
+            ->actingAs($this->user)
+            ->post(route('admin.badges.store'), [
+                'name' => $want->name,
+                'description' => $want->description,
+                'required_repetitions' => $want->required_repetitions,
+                'active' => $want->active,
+                'actuators' => $want->actuators,
+            ])
+            ->assertForbidden();
 
         $this->assertDatabaseMissing(Badge::class, [
             'name' => $want->name,
@@ -138,120 +112,375 @@ class AdminBadgeControllerTest extends TestCase
     }
 
     /** @test */
-    public function show_returns_proper_content()
+    public function admins_should_create_badges(): void
     {
-        /** @var Badge $badge */
-        $badge = Badge::factory()->create();
+        $this->user->role = Roles::Admin;
 
-        $this->get(route('admin.badges.show', $badge))
-            ->assertOk()
-            ->assertViewIs('admin.badge.show')
-            ->assertSee($badge->name);
-    }
+        /** @var Badge $want */
+        $want = Badge::factory()->make();
 
-    /** @test */
-    public function edit_returns_proper_content()
-    {
-        /** @var Badge $badge */
-        $badge = Badge::factory()->create();
+        $this
+            ->actingAs($this->user)
+            ->post(route('admin.badges.store'), [
+                'name' => $want->name,
+                'description' => $want->description,
+                'required_repetitions' => $want->required_repetitions,
+                'active' => $want->active,
+                'actuators' => $want->actuators->value,
+            ])
+            ->assertRedirect(route('admin.badges.index'))
+            ->assertValid();
 
-        $this->get(route('admin.badges.edit', $badge))
-            ->assertOk()
-            ->assertViewIs('admin.badge.edit')
-            ->assertSee($badge->name);
-    }
-
-    /** @test */
-    public function update_edits_an_object()
-    {
-        /** @var Badge $badge */
-        $badge = Badge::factory()->create([
-            'name' => 'gold',
+        $this->assertDatabaseHas(Badge::class, [
+            'name' => $want->name,
+            'description' => $want->description,
+            'required_repetitions' => $want->required_repetitions,
+            'active' => $want->active,
+            'actuators' => $want->actuators,
         ]);
-        $input_data = [
-            'name' => 'silver',
-            'description' => $badge->description,
-            'required_repetitions' => $badge->required_repetitions,
-            'active' => true,
-            'actuators' => $badge->actuators->value,
+    }
+
+    /**
+     * @test
+     * @dataProvider provideWrongDataForBadgeCreation
+     */
+    public function admins_should_get_errors_when_creating_badges_with_wrong_data(
+        array $data,
+        array $errors
+    ): void {
+        $this->user->role = Roles::Admin;
+
+        // Badge to validate unique rules...
+        Badge::factory()->create([
+            'name' => 'my badge',
+        ]);
+
+        /** @var Badge $want */
+        $want = Badge::factory()->make();
+
+        $formData = [
+            'name' => $data['name'] ?? $want->name,
+            'description' => $data['description'] ?? $want->description,
+            'required_repetitions' => $data['required_repetitions'] ?? $want->required_repetitions,
+            'active' => $data['active'] ?? $want->active,
+            'actuators' => $data['actuators'] ?? $want->actuators->value,
         ];
 
-        $this->put(route('admin.badges.update', $badge), $input_data)
+        $this
+            ->actingAs($this->user)
+            ->post(route('admin.badges.store'), $formData)
+            ->assertInvalid($errors);
+
+        $this->assertDatabaseMissing(Badge::class, [
+            'name' => $formData['name'],
+            'description' => $formData['description'],
+        ]);
+    }
+
+    public function provideWrongDataForBadgeCreation(): Generator
+    {
+        yield 'name is empty' => [
+            'data' => [
+                'name' => '',
+            ],
+            'errors' => ['name'],
+        ];
+
+        yield 'name is taken' => [
+            'data' => [
+                'name' => 'my badge',
+            ],
+            'errors' => ['name'],
+        ];
+
+        yield 'description is empty' => [
+            'data' => [
+                'description' => '',
+            ],
+            'errors' => ['description'],
+        ];
+
+        yield 'required_repetitions is empty' => [
+            'data' => [
+                'required_repetitions' => '',
+            ],
+            'errors' => ['required_repetitions'],
+        ];
+
+        yield 'required_repetitions ! an integer' => [
+            'data' => [
+                'required_repetitions' => 'foo',
+            ],
+            'errors' => ['required_repetitions'],
+        ];
+
+        yield 'required_repetitions ! valid' => [
+            'data' => [
+                'required_repetitions' => 0,
+            ],
+            'errors' => ['required_repetitions'],
+        ];
+
+        yield 'active is empty' => [
+            'data' => [
+                'active' => '',
+            ],
+            'errors' => ['active'],
+        ];
+
+        yield 'active ! a boolean' => [
+            'data' => [
+                'active' => 'foo',
+            ],
+            'errors' => ['active'],
+        ];
+
+        yield 'actuators is empty' => [
+            'data' => [
+                'actuators' => '',
+            ],
+            'errors' => ['actuators'],
+        ];
+
+        yield 'actuators ! valid' => [
+            'data' => [
+                'actuators' => 'foo',
+            ],
+            'errors' => ['actuators'],
+        ];
+    }
+
+    /** @test */
+    public function players_should_not_see_any_badge(): void
+    {
+        $want = Badge::factory()->create();
+
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.show', $want))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function admins_should_see_any_badge(): void
+    {
+        $this->user->role = Roles::Admin;
+
+        $badge = Badge::factory()->create();
+
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.show', $badge))
+            ->assertSuccessful()
+            ->assertViewIs('admin.badge.show')
+            ->assertViewHas('badge', $badge);
+    }
+
+    /** @test */
+    public function players_should_not_see_the_edit_badge_form(): void
+    {
+        $want = Badge::factory()->create();
+
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.edit', $want))
+            ->assertForbidden();
+    }
+
+    /** @test */
+    public function admins_should_see_the_edit_badge_form(): void
+    {
+        $this->user->role = Roles::Admin;
+
+        $badge = Badge::factory()->create();
+
+        $this
+            ->actingAs($this->user)
+            ->get(route('admin.badges.edit', $badge))
+            ->assertSuccessful()
+            ->assertViewIs('admin.badge.edit')
+            ->assertViewHas('badge', $badge);
+    }
+
+    /** @test */
+    public function players_should_not_update_badges(): void
+    {
+        $want = Badge::factory()->create();
+
+        $this
+            ->actingAs($this->user)
+            ->put(route('admin.badges.update', $want), [])
+            ->assertForbidden();
+
+        $this->assertModelExists($want);
+    }
+
+    /** @test */
+    public function admins_should_update_badges(): void
+    {
+        $this->user->role = Roles::Admin;
+
+        /** @var Badge $badge */
+        $badge = Badge::factory()->create();
+
+        /** @var Badge $want */
+        $want = Badge::factory()->make();
+
+        $this
+            ->actingAs($this->user)
+            ->put(route('admin.badges.update', $badge), [
+                'name' => $want->name,
+                'description' => $want->description,
+                'required_repetitions' => $want->required_repetitions,
+                'active' => $want->active,
+                'actuators' => $want->actuators->value,
+            ])
             ->assertRedirect(route('admin.badges.index'))
-            ->assertSessionHasNoErrors()
-            ->assertSessionHas('success');
+            ->assertValid();
 
         $this->assertDatabaseHas(Badge::class, [
             'id' => $badge->id,
-            'name' => 'silver',
-            'description' => $badge->description,
-            'required_repetitions' => $badge->required_repetitions,
-            'active' => true,
-            'actuators' => $badge->actuators->value,
+            'name' => $want->name,
+            'description' => $want->description,
+            'required_repetitions' => $want->required_repetitions,
+            'active' => $want->active,
+            'actuators' => $want->actuators->value,
         ]);
     }
 
-    /** @test */
-    public function update_returns_errors_on_invalid_data()
-    {
-        /** @var Badge $badge */
-        $badge = Badge::factory()->create([
-            'name' => 'gold',
+    /**
+     * @test
+     * @dataProvider provideWrongDataForBadgeModification
+     */
+    public function admins_should_get_errors_when_updating_badges_with_wrong_data(
+        array $data,
+        array $errors
+    ): void {
+        $this->user->role = Roles::Admin;
+
+        // Badge to validate unique rules...
+        Badge::factory()->create([
+            'name' => 'my badge',
         ]);
-        $input_data = [
-            'name' => 'silver',
+
+        /** @var Badge $level */
+        $badge = Badge::factory()->create();
+
+        /** @var Badge $want */
+        $want = Badge::factory()->make();
+
+        $formData = [
+            'name' => $data['name'] ?? $want->name,
+            'description' => $data['description'] ?? $want->description,
+            'required_repetitions' => $data['required_repetitions'] ?? $want->required_repetitions,
+            'active' => $data['active'] ?? $want->active,
+            'actuators' => $data['actuators'] ?? $want->actuators->value,
         ];
 
-        $this->put(route('admin.badges.update', $badge), $input_data)
-            ->assertSessionHasErrors()
-            ->assertSessionHas('errors');
+        $this
+            ->actingAs($this->user)
+            ->put(route('admin.badges.update', $badge), $formData)
+            ->assertInvalid($errors);
 
-        $badge->refresh();
-        $this->assertEquals('gold', $badge->name);
+        $this->assertModelExists($badge);
+    }
+
+    public function provideWrongDataForBadgeModification(): Generator
+    {
+        yield 'name is empty' => [
+            'data' => [
+                'name' => '',
+            ],
+            'errors' => ['name'],
+        ];
+
+        yield 'name is taken' => [
+            'data' => [
+                'name' => 'my badge',
+            ],
+            'errors' => ['name'],
+        ];
+
+        yield 'description is empty' => [
+            'data' => [
+                'description' => '',
+            ],
+            'errors' => ['description'],
+        ];
+
+        yield 'required_repetitions is empty' => [
+            'data' => [
+                'required_repetitions' => '',
+            ],
+            'errors' => ['required_repetitions'],
+        ];
+
+        yield 'required_repetitions ! an integer' => [
+            'data' => [
+                'required_repetitions' => 'foo',
+            ],
+            'errors' => ['required_repetitions'],
+        ];
+
+        yield 'required_repetitions ! valid' => [
+            'data' => [
+                'required_repetitions' => 0,
+            ],
+            'errors' => ['required_repetitions'],
+        ];
+
+        yield 'active is empty' => [
+            'data' => [
+                'active' => '',
+            ],
+            'errors' => ['active'],
+        ];
+
+        yield 'active ! a boolean' => [
+            'data' => [
+                'active' => 'foo',
+            ],
+            'errors' => ['active'],
+        ];
+
+        yield 'actuators is empty' => [
+            'data' => [
+                'actuators' => '',
+            ],
+            'errors' => ['actuators'],
+        ];
+
+        yield 'actuators ! valid' => [
+            'data' => [
+                'actuators' => 'foo',
+            ],
+            'errors' => ['actuators'],
+        ];
     }
 
     /** @test */
-    public function delete_returns_proper_content()
+    public function players_should_not_delete_levels(): void
     {
-        /** @var Badge $badge */
         $badge = Badge::factory()->create();
 
-        $this->get(route('admin.badges.delete', $badge))
-            ->assertOk()
-            ->assertViewIs('admin.badge.delete')
-            ->assertSee($badge->name);
+        $this
+            ->actingAs($this->user)
+            ->delete(route('admin.badges.destroy', $badge))
+            ->assertForbidden();
     }
 
     /** @test */
-    public function destroy_deletes_an_object()
+    public function admins_should_delete_badges(): void
     {
-        /** @var Badge $badge */
+        $this->user->role = Roles::Admin;
+
         $badge = Badge::factory()->create();
 
-        $this->delete(route('admin.badges.destroy', $badge))
-            ->assertRedirect(route('admin.badges.index'))
-            ->assertSessionHasNoErrors()
-            ->assertSessionHas('success');
+        $this
+            ->actingAs($this->user)
+            ->delete(route('admin.badges.destroy', $badge))
+            ->assertRedirect(route('admin.badges.index'));
 
         $this->assertSoftDeleted($badge);
-    }
-
-    /** @test */
-    public function data_returns_proper_content()
-    {
-        Badge::factory()->count(3)->create();
-
-        $this->withoutMiddleware(OnlyAjax::class)
-            ->get(route('admin.badges.data'))
-            ->assertJsonCount(3, 'data');
-    }
-
-    /** @test */
-    public function data_fails_for_non_ajax_calls()
-    {
-        Badge::factory()->count(3)->create();
-
-        $this->get(route('admin.badges.data'))
-            ->assertForbidden();
     }
 }
