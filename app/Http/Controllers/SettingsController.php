bccec16c61337c3d9549ab8d0f82dad755188688
pacoorozco <paco@pacoorozco.info>
2022-07-10 09:53:36 +0200

    Fix broken test after refactor
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Gamify - Gamification platform to implement any serious game mechanic.
+ *
+ * Copyright (c) 2018 by Paco Orozco <paco@pacoorozco.info>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Some rights reserved. See LICENSE and AUTHORS files.
+ *
+ * @author             Paco Orozco <paco@pacoorozco.info>
+ * @copyright          2018 Paco Orozco
+ * @license            GPL-3.0 <http://spdx.org/licenses/GPL-3.0>
+ *
+ * @link               https://github.com/pacoorozco/gamify-laravel
+ */
+
+namespace Gamify\Http\Controllers;
+
+use Gamify\Actions\UpdatePasswordAction;
+use Gamify\Events\UserProfileUpdated;
+use Gamify\Http\Requests\UpdatePasswordRequest;
+use Gamify\Http\Requests\UserProfileUpdateRequest;
+use Gamify\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class SettingsController extends Controller
+{
+    public function index(): View
+    {
+        $user = Auth::user()->refresh();
+
+        return view('profile.show')
+            ->with('user', $user);
+    }
+
+    public function update(UserProfileUpdateRequest $request): RedirectResponse
+    {
+        $user = Auth::user()->refresh();
+
+        $user
+            ->profile
+            ->update($request->safe());
+
+        UserProfileUpdated::dispatchIf($user->profile->wasChanged(), $user);
+
+        return redirect()->action(static::class, 'index')
+            ->with('success', __('user/messages.settings_updated'));
+    }
+}
