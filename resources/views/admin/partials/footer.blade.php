e3713a2a053a1caf0bf09cb95acdd41a09700398
pacoorozco <paco@pacoorozco.info>
2022-10-10 15:38:02 +0200

    Fix the footer
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,8 +1,6 @@
 <footer class="main-footer">
-    <div class="container">
-        <div class="pull-right hidden-xs">
-            Just play. Have fun. Enjoy the game.
-        </div>
-        Powered by <a href="https://github.com/pacoorozco/gamify-laravel" rel="nofollow">gamify v{{ config('app.version') }}</a>
+    <div class="pull-right hidden-xs">
+        Just play. Have fun. Enjoy the game.
     </div>
+    <strong>Powered by <a href="https://github.com/pacoorozco/gamify-laravel" rel="nofollow">gamify v{{ config('app.version') }}</a>.</strong>
 </footer>
