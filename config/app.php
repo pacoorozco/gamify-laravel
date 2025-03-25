6c2287898a190e54f12ae28caaad0a09942ffd59
pacoorozco <paco@pacoorozco.info>
2025-03-25 09:33:14 +0100

    Update config files for laravel v11
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- config/app.php
+++ config/app.php
@@ -92,7 +92,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -123,58 +123,60 @@ return [
     'timezone' => env('APP_TIMEZONE', 'UTC'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Application Locale Configuration
-    |--------------------------------------------------------------------------
-    |
-    | The application locale determines the default locale that will be used
-    | by the translation service provider. You are free to set this value
-    | to any of the locales which will be supported by the application.
-    |
-    */
+     |--------------------------------------------------------------------------
+     | Application Locale Configuration
+     |--------------------------------------------------------------------------
+     |
+     | The application locale determines the default locale that will be used
+     | by Laravel's translation / localization methods. This option can be
+     | set to any locale for which you plan to have translation strings.
+     |
+     */
 
-    'locale' => 'en',
+    'locale' => env('APP_LOCALE', 'en'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Fallback Locale
-    |--------------------------------------------------------------------------
-    |
-    | The fallback locale determines the locale to use when the current one
-    | is not available. You may change the value to correspond to any of
-    | the language folders that are provided through your application.
-    |
-    */
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
-    'fallback_locale' => 'en',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Faker Locale
-    |--------------------------------------------------------------------------
-    |
-    | This locale will be used by the Faker PHP library when generating fake
-    | data for your database seeds. For example, this will be used to get
-    | localized telephone numbers, street address information and more.
-    |
-    */
-
-    'faker_locale' => 'en_US',
+    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
     /*
     |--------------------------------------------------------------------------
     | Encryption Key
     |--------------------------------------------------------------------------
     |
-    | This key is used by the Illuminate encrypter service and should be set
-    | to a random, 32 character string, otherwise these encrypted strings
-    | will not be safe. Please do this before deploying an application!
+    | This key is utilized by Laravel's encryption services and should be set
+    | to a random, 32 character string to ensure that all encrypted values
+    | are secure. You should do this prior to deploying the application.
     |
     */
 
+    'cipher' => 'AES-256-CBC',
+
     'key' => env('APP_KEY'),
 
-    'cipher' => 'AES-256-CBC',
+    'previous_keys' => [
+        ...array_filter(
+            explode(',', env('APP_PREVIOUS_KEYS', ''))
+        ),
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Maintenance Mode Driver
+     |--------------------------------------------------------------------------
+     |
+     | These configuration options determine the driver used to determine and
+     | manage Laravel's "maintenance mode" status. The "cache" driver will
+     | allow maintenance mode to be controlled across multiple machines.
+     |
+     | Supported drivers: "file", "cache"
+     |
+     */
+
+    'maintenance' => [
+        'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
+        'store' => env('APP_MAINTENANCE_STORE', 'database'),
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -284,8 +286,6 @@ return [
         /*
          * Third Party Alias
          */
-        'Form' => Collective\Html\FormFacade::class, // Forms
-        'Html' => Collective\Html\HtmlFacade::class, // HTML
         'Socialite' => Laravel\Socialite\Facades\Socialite::class, // Socialite
 
     ],
--- config/auth.php
+++ config/auth.php
@@ -30,15 +30,15 @@ return [
     | Authentication Defaults
     |--------------------------------------------------------------------------
     |
-    | This option controls the default authentication "guard" and password
-    | reset options for your application. You may change these defaults
+    | This option defines the default authentication "guard" and password
+    | reset "broker" for your application. You may change these values
     | as required, but they're a perfect start for most applications.
     |
     */
 
     'defaults' => [
-        'guard' => 'web',
-        'passwords' => 'users',
+        'guard' => env('AUTH_GUARD', 'web'),
+        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
     /*
@@ -48,13 +48,13 @@ return [
     |
     | Next, you may define every authentication guard for your application.
     | Of course, a great default configuration has been defined for you
-    | here which uses session storage and the Eloquent user provider.
+    | which utilizes session storage plus the Eloquent user provider.
     |
-    | All authentication drivers have a user provider. This defines how the
+    | All authentication guards have a user provider, which defines how the
     | users are actually retrieved out of your database or other storage
-    | mechanisms used by this application to persist your user's data.
+    | system used by the application. Typically, Eloquent is utilized.
     |
-    | Supported: "session", "token"
+    | Supported: "session"
     |
     */
 
@@ -62,12 +62,6 @@ return [
         'web' => [
             'driver' => 'session',
             'provider' => 'users',
-        ],
-
-        'api' => [
-            'driver' => 'token',
-            'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
@@ -91,7 +85,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => Gamify\Models\User::class,
+            'model' => env('AUTH_MODEL', Gamify\Models\User::class),
         ],
 
         // 'users' => [
@@ -102,39 +96,27 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Login Throttling
-    |--------------------------------------------------------------------------
-    |
-    | If there are more than 'max_attempts' request in 60 seconds, then the
-    | user should wait for 'decay_minutes' time before trying again.
-    |
-     */
-    'login' => [
-        // Maximum number of attempts to allow.
-        'max_attempts' => '5',
-        // Number of minutes to throttle for.
-        'decay_minutes' => '1',
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
     | Resetting Passwords
     |--------------------------------------------------------------------------
     |
-    | You may specify multiple password reset configurations if you have more
-    | than one user table or model in the application and you want to have
-    | separate password reset settings based on the specific user types.
+    | These configuration options specify the behavior of Laravel's password
+    | reset functionality, including the table utilized for token storage
+    | and the user provider that is invoked to actually retrieve users.
     |
-    | The expire time is the number of minutes that the reset token should be
+    | The expiry time is the number of minutes that each reset token will be
     | considered valid. This security feature keeps tokens short-lived so
     | they have less time to be guessed. You may change this as needed.
+    |
+    | The throttle setting is the number of seconds a user must wait before
+    | generating more password reset tokens. This prevents the user from
+    | quickly generating a very large amount of password reset tokens.
     |
     */
 
     'passwords' => [
         'users' => [
             'provider' => 'users',
-            'table' => 'password_resets_tokens',
+            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
             'expire' => 60,
             'throttle' => 60,
         ],
@@ -151,6 +133,6 @@ return [
     |
     */
 
-    'password_timeout' => 10800,
+    'password_timeout' => env('AUTH_PASSWORD_TIMEOUT', 10800),
 
 ];
