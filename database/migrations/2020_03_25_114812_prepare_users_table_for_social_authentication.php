889c8e0be3dc77af57d7b369d2d5c55d2c047317
pacoorozco <paco@pacoorozco.info>
2025-04-01 11:10:02 +0200

    refactor migrations to make them compliant with laravel 11
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- database/migrations/2020_03_25_114812_prepare_users_table_for_social_authentication.php
+++ database/migrations/2020_03_25_114812_prepare_users_table_for_social_authentication.php
@@ -27,22 +27,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
             // Making email and password nullable
-            $table->string('email')->nullable()->change();
-            $table->string('password')->nullable()->change();
+            $table->string('email')
+                ->unique()
+                ->nullable()
+                ->change();
+            $table->string('password')
+                ->nullable()
+                ->change();
         });
     }
 
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('email')->nullable(false)->change();
-            $table->string('password')->nullable(false)->change();
+            $table->string('email')
+                ->unique()
+                ->nullable(false)
+                ->change();
+            $table->string('password')
+                ->nullable(false)
+                ->change();
         });
     }
 };
